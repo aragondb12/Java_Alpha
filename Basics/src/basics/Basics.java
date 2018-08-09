88213c9d1512d5587d9e9a1800ccb3b1cdddf98c
aragondb12 <ariadne_2126@hotmail.com>
2018-08-09 15:28:39 -0500

    ejercicios presentacion 1

--- Basics/src/basics/Basics.java
+++ Basics/src/basics/Basics.java
@@ -8,7 +8,40 @@ package basics;
 public class Basics {
 
     public static void main(String[] args) {
+        int opc=2, n=3;
+        
         System.out.println("Hola Mundo");
-    }
+         
+        switch(opc){
+            case 1:
+                System.out.println("Uno");
+                break;
+            case 2:
+                System.out.println("Dos");
+                break;
+            case 3:
+                System.out.println("Tres");
+                break;
+        }
+      
+        System.out.println("Ejercicio 1 Sesion 1");
+         for (int i = 0; i < n; i++) {
+             for(int j = 0;j <= i;j++){
+             System.out.print("*");
+             }
+             System.out.println("");
+         }
+         System.out.println("Ejercicio 2 while");
+         int fact = 1, cont =1;
+         while (fact <10 ){
+             System.out.println("Fact("+ cont + ")="+fact);
+             cont++;
+             fact=fact*cont;
+         }
+        int num=3;
+        for (int i = 1; i <=num; i++) {
+            System.out.println(num=num*i);
+        }
+    } 
     
 }
