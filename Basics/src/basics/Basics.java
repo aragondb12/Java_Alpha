7d8f75a3e01fdeb01f91d6498273ce775b5a04a4
aragondb12 <ariadne_2126@hotmail.com>
2018-08-09 12:29:36 -0500

    Hola Mundogit add .!

---
+++ Basics/src/basics/Basics.java
@@ -0,0 +1,14 @@
+
+package basics;
+
+/**
+ *
+ * @author aragondb12
+ */
+public class Basics {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
